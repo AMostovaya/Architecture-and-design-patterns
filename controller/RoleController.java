c078ea3f912dafd0357bb25c4ebd5d3b593c2061
Alexandra Mostovaya <31797807+AMostovaya@users.noreply.github.com>
2020-09-14 15:36:11 +0600

    Add files via upload

---
+++ controller/RoleController.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Role;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import ru.geekbrains.mapper.RoleMapper;
+
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Controller
+public class RoleController {
+
+    private final RoleMapper roleMapper;
+
+    @Autowired
+    public RoleController(RoleMapper roleMapper) {
+        this.roleMapper = roleMapper;
+    }
+
+    public String roleList(Model model) throws SQLException {
+        List<Role> rolePage = roleMapper.findAll();
+        model.addAttribute("rolesPage", rolePage);
+        return "roles";
+    }
+}
---
+++ mapper/UserRoleMapper.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.mapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.acls.model.NotFoundException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Service
+public class UserRoleMapper {
+
+    private Connection connection;
+
+    public UserRoleMapper(@Autowired DataSource dataSource) {
+        try {
+            this.connection = dataSource.getConnection();
+        } catch (SQLException ex) {
+            ex.getStackTrace();
+        }
+    }
+
+    public User findById(Long id) throws SQLException, NotFoundException {
+        PreparedStatement statement = connection.prepareStatement(
+                "SELECT user_id, role_id FROM users_roles WHERE id = ?");
+        statement.setLong(1, id);
+        try (ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setPassword(resultSet.getString(3));
+                return user;
+            }
+        }
+        throw new NotFoundException("User is not found");
+    }
+}
